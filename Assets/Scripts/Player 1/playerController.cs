ea0eea65a09c4c5e9eddd15070801781e35b924c
t0rchw00d <twilson170@gmail.com>
2019-07-14 16:25:53 +1000

    Added Jump counter, fixed layering + tags + polygon colliders instead of stacked box colliders

---
+++ Assets/Scripts/Player 1/playerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public LayerMask ground;
+
+    public float moveSpeed = 5.0f;
+    public float playerHealth = 100.00f;
+    public int jumpsLeft = 2;
+    private int _jumpsLeft;
+    private bool onGround;
+    private Rigidbody2D PlayerChar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_jumpsLeft = jumpsLeft;
+        PlayerChar = gameObject.GetComponent<Rigidbody2D>();
+        if (PlayerChar == null)
+        {
+            Debug.LogError("PLAYER: can't find rigidbody");
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        playerHealth -= damage;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "ground")
+        {
+            //Debug.Log("Touched ground! " + _jumpsLeft);
+            onGround = true;
+            if (onGround)
+            {
+               // Debug.Log(_jumpsLeft + " " + onGround);
+                _jumpsLeft = jumpsLeft;
+            } else
+            {
+               // Debug.Log("Not grounded!");
+            }
+            onGround = false;
+        }     
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalMovement = 0.0f;
+        float verticalMovement = 0.0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontalMovement = -moveSpeed * 20 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            horizontalMovement = moveSpeed * 20 * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_jumpsLeft > 0)
+            {
+                verticalMovement = moveSpeed * 500 * Time.deltaTime;
+               // Debug.Log(_jumpsLeft + " / " + jumpsLeft);
+                _jumpsLeft--;
+               // Debug.Log(_jumpsLeft + " / " + jumpsLeft + " post index");
+            }
+            else
+            {
+               // Debug.Log("No Jumps left!");
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            verticalMovement = -moveSpeed * 1500 * Time.deltaTime;
+        }
+
+        Vector2 movementDirection = new Vector2(horizontalMovement, verticalMovement);
+        PlayerChar.AddForce(movementDirection);
+
+        onGround = false;
+        
+    }
+}
---
+++ Assets/Scripts/Player 2/player2Controls.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2Controls : MonoBehaviour
+{
+    //Movement Vars
+    public float moveSpeed = 5.0f;
+    public float playerHealth = 100.00f;
+    public int jumpsLeft = 2;
+    private Rigidbody2D PlayerChar;
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerChar = gameObject.GetComponent<Rigidbody2D>();
+        if (PlayerChar == null)
+        {
+            Debug.LogError("PLAYER: can't find rigidbody");
+        }
+    }
+        
+    public void TakeDamage(float damage)
+    {
+        playerHealth -= damage;
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //LookAtMyMouse();
+        float horizontalMovement = 0.0f;
+        float verticalMovement = 0.0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            horizontalMovement = -moveSpeed * 20 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            horizontalMovement = moveSpeed * 20 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            verticalMovement = moveSpeed * 20 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            verticalMovement = -moveSpeed * 20 * Time.deltaTime;
+        }
+
+        Vector2 movementDirection = new Vector2(horizontalMovement, verticalMovement);
+        PlayerChar.AddForce(movementDirection);
+
+
+    }
+}
