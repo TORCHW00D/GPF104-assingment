6ef04511dbcc506dae1e0d7352ba03574528469c
t0rchw00d <twilson170@gmail.com>
2019-07-14 16:54:30 +1000

    Mouse reticle added, enemy sprites added

---
+++ Assets/Scripts/Game Manager/MouseReticle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseReticle : MonoBehaviour {
+    Vector3 MousePos;
+	// Use this for initialization
+	void Start () {
+        UnityEngine.Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MousePos = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        this.transform.position = new Vector3(MousePos.x, MousePos.y, 0) ;
+	}
+}
