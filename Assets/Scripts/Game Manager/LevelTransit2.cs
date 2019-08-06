a82c57604d28672897241af299b7b217885df30f
t0rchw00d <twilson170@gmail.com>
2019-08-06 16:08:26 +1000

    Fixed colliders, ground materials and menu screens -- jump still broken!

--- Assets/Scripts/Game Manager/LevelTransit2.cs
+++ Assets/Scripts/Game Manager/LevelTransit2.cs
@@ -9,7 +9,7 @@ public class LevelTransit2 : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if (collision.gameObject.tag == "Finish")
+        if (collision.gameObject.tag == "Player1")
         {
             Debug.Log("Level completed!!");
             SceneManager.LoadScene("Level2");
